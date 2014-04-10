0c5b49714afeacdcdaaa29663c060de7efd3a72e
Matthew Landauer <matthew@openaustralia.org>
2014-04-10 18:59:10 +1000

    Now it's something

---
+++ README.md
@@ -0,0 +1,13 @@
+# An amusing experiment to try on myself
+
+This is a new kind of time. One that is different on every point on earth and different on every day.
+
+The idea is simple.
+
+The sun rises at 6 AM and sets at 6 PM. It does this every day wherever on earth you are.
+
+This is really an experiment to see what a different sense of time means. A second can be shorter or longer depending on the season but you are inextricably linked to the world via the sunrise and sunset.
+
+So right now all this little bit of code does is give me the new time.
+
+But for the time being it only works in Katoomba, Australia.
--- time.rb
+++ time.rb
@@ -5,9 +5,38 @@ latitude = -33.714955
 longitude = 150.311407
 tz = "Australia/Sydney"
 
-c = SolarEventCalculator.new(Date.today, latitude, longitude)
+yesterday = SolarEventCalculator.new(Date.today - 1, latitude, longitude)
+today = SolarEventCalculator.new(Date.today, latitude, longitude)
+tomorrow = SolarEventCalculator.new(Date.today + 1, latitude, longitude)
 
-sunrise = c.compute_official_sunrise(tz)
-sunset = c.compute_official_sunset(tz)
+sunset_yesterday = yesterday.compute_official_sunset(tz)
+sunrise_today = today.compute_official_sunrise(tz)
+sunset_today = today.compute_official_sunset(tz)
+sunrise_tomorrow = tomorrow.compute_official_sunrise(tz)
 
-puts "Official sunrise: #{sunrise} and sunset: #{sunset}"
+time = DateTime.now
+
+if time < sunrise_today
+  fraction = (time - sunset_yesterday).to_f / (sunrise_today - sunset_yesterday)
+  seconds = (18 + fraction * 12) * 60 * 60
+elsif time < sunset_today
+  fraction = (time - sunrise_today).to_f / (sunset_today - sunrise_today)
+  seconds = (6 + fraction * 12) * 60 * 60
+else
+  fraction = (time - sunset_today).to_f / (sunrise_tomorrow - sunset_today)
+  seconds = (18 + fraction * 12) * 60 * 60
+end
+
+fraction = seconds - seconds.floor
+seconds = seconds.floor
+minutes = seconds / 60
+seconds -= minutes * 60
+hours = minutes / 60
+minutes -= hours * 60
+hours -= 24 if hours >= 24
+
+if hours > 12
+  puts "%i:%02i pm" % [hours - 12, minutes]
+else
+  puts "%i:%02i am" % [hours, minutes]
+end
