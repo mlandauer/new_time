b34b61c10d03cd693e57843e950a7442d3a8ec32
Matthew Landauer <matthew@oaf.org.au>
2015-01-28 18:32:48 +1100

    Extract method

--- time.rb
+++ time.rb
@@ -9,21 +9,23 @@ class NewTime
   end
 
   def self.current_time(latitude, longitude, tz)
-    time = DateTime.now
+    convert(DateTime.now, latitude, longitude, tz)
+  end
 
-    yesterday = SolarEventCalculator.new(time.to_date - 1, latitude, longitude)
-    today = SolarEventCalculator.new(time.to_date, latitude, longitude)
-    tomorrow = SolarEventCalculator.new(time.to_date + 1, latitude, longitude)
+  def self.convert(date_time, latitude, longitude, tz)
+    yesterday = SolarEventCalculator.new(date_time.to_date - 1, latitude, longitude)
+    today = SolarEventCalculator.new(date_time.to_date, latitude, longitude)
+    tomorrow = SolarEventCalculator.new(date_time.to_date + 1, latitude, longitude)
 
     sunset_yesterday = yesterday.compute_official_sunset(tz)
     sunrise_today = today.compute_official_sunrise(tz)
     sunset_today = today.compute_official_sunset(tz)
     sunrise_tomorrow = tomorrow.compute_official_sunrise(tz)
 
-    if time < sunrise_today
+    if date_time < sunrise_today
       start, finish = sunset_yesterday, sunrise_today
       start_hour = 18
-    elsif time < sunset_today
+    elsif date_time < sunset_today
       start, finish = sunrise_today, sunset_today
       start_hour = 6
     else
@@ -31,7 +33,7 @@ class NewTime
       start_hour = 18
     end
 
-    seconds = (start_hour + (time - start).to_f / (finish - start) * 12) * 60 * 60
+    seconds = (start_hour + (date_time - start).to_f / (finish - start) * 12) * 60 * 60
 
     fractional = seconds - seconds.floor
     seconds = seconds.floor
