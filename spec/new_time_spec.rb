88e1ae8cce7b03bc4011a20d8888273d99fdd23a
Matthew Landauer <matthew@oaf.org.au>
2015-01-31 18:00:50 +1100

    Add some tests for a northern hemisphere point

--- spec/new_time_spec.rb
+++ spec/new_time_spec.rb
@@ -5,7 +5,59 @@ describe NewTime do
     expect(NewTime::VERSION).not_to be nil
   end
 
-  context "For a fixed place" do
+  context "For a fixed place in the northern hemisphere" do
+    let(:latitude)  { 51.829731 }
+    let(:longitude) { -0.860455 }
+    let(:tz)        { "Europe/London" }
+
+    describe ".convert" do
+      context "2015-01-31" do
+        it "00:00:01" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T00:00:01+00:00"), latitude, longitude, tz).to_s).to eq "2015-01-30 11:47 pm"
+        end
+
+        it "03:00:00" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T03:00:00+00:00"), latitude, longitude, tz).to_s).to eq "2015-01-31 2:11 am"
+        end
+
+        it "14:32:10" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T14:32:10+00:00"), latitude, longitude, tz).to_s).to eq "2015-01-31 2:58 pm"
+        end
+
+        it "20:10:00" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T20:10:00+00:00"), latitude, longitude, tz).to_s).to eq "2015-01-31 8:41 pm"
+        end
+
+        it "23:59:59" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T23:59:59+00:00"), latitude, longitude, tz).to_s).to eq "2015-01-31 11:47 pm"
+        end
+      end
+
+      context "2015-07-31" do
+        it "00:00:01" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-07-31T00:00:01+00:00"), latitude, longitude, tz).to_s).to eq "2015-07-30 11:46 pm"
+        end
+
+        it "03:00:00" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-07-31T03:00:00+00:00"), latitude, longitude, tz).to_s).to eq "2015-07-31 4:02 am"
+        end
+
+        it "14:32:10" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-07-31T14:32:10+00:00"), latitude, longitude, tz).to_s).to eq "2015-07-31 1:50 pm"
+        end
+
+        it "20:10:00" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-07-31T20:10:00+00:00"), latitude, longitude, tz).to_s).to eq "2015-07-31 6:21 pm"
+        end
+
+        it "23:59:59" do
+          expect(NewTime::NewTime.convert(DateTime.parse("2015-07-31T23:59:59+00:00"), latitude, longitude, tz).to_s).to eq "2015-07-31 11:45 pm"
+        end
+      end
+    end
+  end
+
+  context "For a fixed place in the southern hemisphere" do
     let(:latitude)  { -33.714955 }
     let(:longitude) { 150.311407 }
     let(:tz)        { "Australia/Sydney" }
