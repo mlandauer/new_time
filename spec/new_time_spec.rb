0412946b9caebe7c602067b69d16b399d92ada4a
Matthew Landauer <matthew@oaf.org.au>
2015-01-31 16:49:56 +1100

    Add more times in tests

--- spec/new_time_spec.rb
+++ spec/new_time_spec.rb
@@ -5,18 +5,32 @@ describe NewTime do
     expect(NewTime::VERSION).not_to be nil
   end
 
-  context "For a fixed time and place" do
+  context "For a fixed place" do
     let(:latitude)  { -33.714955 }
     let(:longitude) { 150.311407 }
     let(:tz)        { "Australia/Sydney" }
-    let(:date_time) { DateTime.parse("2015-01-31T14:32:10+11:00") }
 
     describe ".convert" do
-      let(:t) { NewTime::NewTime.convert(date_time, latitude, longitude, tz) }
-      it { expect(t.hours).to eq 13 }
-      it { expect(t.minutes).to eq 10 }
-      it { expect(t.seconds).to eq 23 }
-      it { expect(t.fractional).to be_within(0.00000001).of(0.999999999992724) }
+      it "00:00:01" do
+        expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T00:00:01+11:00"), latitude, longitude, tz).to_s).to eq "10:35 pm"
+      end
+
+      it "03:00:00" do
+        expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T03:00:00+11:00"), latitude, longitude, tz).to_s).to eq "2:06 am"
+      end
+
+      it "14:32:10" do
+        expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T14:32:10+11:00"), latitude, longitude, tz).to_s).to eq "1:10 pm"
+      end
+
+      it "20:10:00" do
+        expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T20:10:00+11:00"), latitude, longitude, tz).to_s).to eq "6:07 pm"
+      end
+
+      it "23:59:59" do
+        expect(NewTime::NewTime.convert(DateTime.parse("2015-01-31T23:59:59+11:00"), latitude, longitude, tz).to_s).to eq "10:35 pm"
+      end
+
     end
   end
 end
