6829bdba4d278237a237a734fef22d5eac383f7b
Matthew Landauer <matthew@oaf.org.au>
2015-01-31 14:29:29 +1100

    Add test skeleton

--- lib/new_time.rb
+++ lib/new_time.rb
@@ -1,55 +1,57 @@
 require 'solareventcalculator'
 
-class NewTime
-  attr_accessor :hours, :minutes, :seconds, :fractional
+module NewTime
+  class NewTime
+    attr_accessor :hours, :minutes, :seconds, :fractional
 
-  def initialize(hours, minutes, seconds, fractional)
-    @hours, @minutes, @seconds, @fractional = hours, minutes, seconds, fractional
-  end
-
-  def self.current_time(latitude, longitude, tz)
-    convert(DateTime.now, latitude, longitude, tz)
-  end
-
-  def self.convert(date_time, latitude, longitude, tz)
-    yesterday = SolarEventCalculator.new(date_time.to_date - 1, latitude, longitude)
-    today = SolarEventCalculator.new(date_time.to_date, latitude, longitude)
-    tomorrow = SolarEventCalculator.new(date_time.to_date + 1, latitude, longitude)
-
-    sunset_yesterday = yesterday.compute_official_sunset(tz)
-    sunrise_today = today.compute_official_sunrise(tz)
-    sunset_today = today.compute_official_sunset(tz)
-    sunrise_tomorrow = tomorrow.compute_official_sunrise(tz)
-
-    if date_time < sunrise_today
-      start, finish = sunset_yesterday, sunrise_today
-      start_hour = 18
-    elsif date_time < sunset_today
-      start, finish = sunrise_today, sunset_today
-      start_hour = 6
-    else
-      start, finish = sunset_today, sunrise_tomorrow
-      start_hour = 18
+    def initialize(hours, minutes, seconds, fractional)
+      @hours, @minutes, @seconds, @fractional = hours, minutes, seconds, fractional
     end
 
-    seconds = (start_hour + (date_time - start).to_f / (finish - start) * 12) * 60 * 60
+    def self.current_time(latitude, longitude, tz)
+      convert(DateTime.now, latitude, longitude, tz)
+    end
 
-    fractional = seconds - seconds.floor
-    seconds = seconds.floor
-    minutes = seconds / 60
-    seconds -= minutes * 60
-    hours = minutes / 60
-    minutes -= hours * 60
-    hours -= 24 if hours >= 24
+    def self.convert(date_time, latitude, longitude, tz)
+      yesterday = SolarEventCalculator.new(date_time.to_date - 1, latitude, longitude)
+      today = SolarEventCalculator.new(date_time.to_date, latitude, longitude)
+      tomorrow = SolarEventCalculator.new(date_time.to_date + 1, latitude, longitude)
 
-    NewTime.new(hours, minutes, seconds, fractional)
-  end
+      sunset_yesterday = yesterday.compute_official_sunset(tz)
+      sunrise_today = today.compute_official_sunrise(tz)
+      sunset_today = today.compute_official_sunset(tz)
+      sunrise_tomorrow = tomorrow.compute_official_sunrise(tz)
 
-  def to_s
-    if hours > 12
-      "%i:%02i pm" % [hours - 12, minutes]
-    else
-      "%i:%02i am" % [hours, minutes]
+      if date_time < sunrise_today
+        start, finish = sunset_yesterday, sunrise_today
+        start_hour = 18
+      elsif date_time < sunset_today
+        start, finish = sunrise_today, sunset_today
+        start_hour = 6
+      else
+        start, finish = sunset_today, sunrise_tomorrow
+        start_hour = 18
+      end
+
+      seconds = (start_hour + (date_time - start).to_f / (finish - start) * 12) * 60 * 60
+
+      fractional = seconds - seconds.floor
+      seconds = seconds.floor
+      minutes = seconds / 60
+      seconds -= minutes * 60
+      hours = minutes / 60
+      minutes -= hours * 60
+      hours -= 24 if hours >= 24
+
+      NewTime.new(hours, minutes, seconds, fractional)
+    end
+
+    def to_s
+      if hours > 12
+        "%i:%02i pm" % [hours - 12, minutes]
+      else
+        "%i:%02i am" % [hours, minutes]
+      end
     end
   end
 end
---
+++ spec/new_time_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe NewTime do
+  it 'has a version number' do
+    expect(NewTime::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
